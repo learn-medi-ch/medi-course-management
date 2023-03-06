f452d208d4218f0ab76883cf1b0c0fb3ea60208c
Martin Studer <martin.studer@medi.ch>
2023-03-06 12:33:11 +0100

    ...

---
+++ provideEnvironmentsTemplate.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+export HTTP_ENDPOINT_SERVER='{"protocol": "http", "host": "localhost", "port": 3100}'
+export HTTP_BACKEND_SERVER='{"protocol": "http", "host": "localhost", "port": 3200}'
